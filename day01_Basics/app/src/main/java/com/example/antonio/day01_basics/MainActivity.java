8066dfd5bd0d7c027753b62cc450f671452f89bf
“antonialq” <1649794366@qq.com>
2019-01-18 23:56:00 +0800

    update

--- day01_Basics/app/src/main/java/com/example/antonio/day01_basics/MainActivity.java
+++ day01_Basics/app/src/main/java/com/example/antonio/day01_basics/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.antonio.day01_basics;
 
+import android.content.Intent;
+import android.graphics.Paint;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+    private void initViews(){
+        mBtnDetail = (Button)findViewById(R.id.btn_detail);
+        Paint paint = mBtnDetail.getPaint();
+        paint.setFlags(paint.getFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
     }
 }
