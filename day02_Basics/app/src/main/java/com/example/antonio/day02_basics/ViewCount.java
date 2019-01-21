811a6f3d9c8ec7dc9d0c95cbbaf6980007241f2e
“antonialq” <1649794366@qq.com>
2019-01-21 10:09:46 +0800

    exercise2 finished

--- day02_Basics/app/src/main/java/com/example/antonio/day02_basics/ViewCount.java
+++ day02_Basics/app/src/main/java/com/example/antonio/day02_basics/ViewCount.java
@@ -16,26 +16,50 @@ public class ViewCount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_count);
-        System.out.print(findViewById(R.id.statistics));
-        getAllViews(this);
+        TextView number = findViewById(R.id.statistics);
+        if (getWindow().getDecorView().getRootView() instanceof ViewGroup){
+//            int num = getChildrenViews((ViewGroup) getWindow().getDecorView().getRootView());
+            int num = countChild(getWindow().getDecorView().getRootView());
+            number.setText(Integer.toString(num));
+        }
     }
-    public void getAllViews(AppCompatActivity act) {
-        List<View> allchildren = new ArrayList<View>();
-        List<View> list = getAllChildViews(act.getWindow().getDecorView(),allchildren);
-        System.out.print(findViewById(R.id.statistics));
+    private int countChild(View view) {
+        if (!(view instanceof ViewGroup))
+            return 1;
+        int counter = 0;
+        ViewGroup viewGroup = (ViewGroup) view;
+        for (int i=0; i<viewGroup.getChildCount(); i++) {
+            counter += countChild(viewGroup.getChildAt(i));
+        }
+        return counter;
     }
 
-    public List<View> getAllChildViews(View view,List<View> allchildren) {
-        if (view instanceof ViewGroup) {
-            ViewGroup vp = (ViewGroup) view;
-            for (int i = 0; i < vp.getChildCount(); i++) {
-                View viewchild = vp.getChildAt(i);
-                allchildren.add(viewchild);
-                //再次 调用本身（递归）
-                allchildren.addAll(getAllChildViews(viewchild,allchildren));
+    public int getChildrenViews(ViewGroup parent){
+        int count = parent.getChildCount();
+        for (int i=0;i<parent.getChildCount();i++){
+            if (parent.getChildAt(i) instanceof ViewGroup){
+                count+=getChildrenViews((ViewGroup) parent.getChildAt(i));
             }
         }
-        return allchildren;
+        return count;
     }
+//    public void getAllViews(AppCompatActivity act) {
+//        List<View> allchildren = new ArrayList<View>();
+//        List<View> list = getAllChildViews(act.getWindow().getDecorView(),allchildren);
+//        System.out.print(findViewById(R.id.statistics));
+//    }
+//
+//    public List<View> getAllChildViews(View view,List<View> allchildren) {
+//        if (view instanceof ViewGroup) {
+//            ViewGroup vp = (ViewGroup) view;
+//            for (int i = 0; i < vp.getChildCount(); i++) {
+//                View viewchild = vp.getChildAt(i);
+//                allchildren.add(viewchild);
+//                //再次 调用本身（递归）
+//                allchildren.addAll(getAllChildViews(viewchild,allchildren));
+//            }
+//        }
+//        return allchildren;
+//    }
 
 }
